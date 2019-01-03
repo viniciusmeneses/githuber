2d14fad2ea6375e8ca835b0bdef1fe9220c0fd6f
viniciusmeneses <vini23hot@gmail.com>
2019-01-03 21:05:01 -0200

    Created welcome page

---
+++ src/pages/Welcome/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import {
+  View, Text, TextInput, TouchableOpacity, StatusBar,
+} from 'react-native';
+
+import styles from './styles';
+
+const Welcome = () => (
+  <View style={styles.container}>
+    <StatusBar barStyle="light-content" />
+    <Text style={styles.title}>Bem-vindo</Text>
+    <Text style={styles.text}>Para continuar precisamos que voce informe seu usuario do Github</Text>
+
+    <View style={styles.form}>
+      <TextInput style={styles.input} autoCapitalize="none" autoCorrect={false} placeholder="Digite seu usuario" underlineColorAndroid="transparent" />
+      <TouchableOpacity style={styles.button} onPress={() => {}}>
+        <Text style={styles.buttonText}>Prosseguir</Text>
+      </TouchableOpacity>
+    </View>
+  </View>
+);
+
+export default Welcome;
